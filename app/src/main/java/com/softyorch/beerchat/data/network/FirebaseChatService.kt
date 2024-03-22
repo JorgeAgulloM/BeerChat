c4173f23fc8e7f8654d3211332f858b610bd0ca3
Jorge1 <agullojorge@gmail.com>
2024-03-22 18:56:50 +0100

    Implemented data model

--- app/src/main/java/com/softyorch/beerchat/data/network/FirebaseChatService.kt
+++ app/src/main/java/com/softyorch/beerchat/data/network/FirebaseChatService.kt
@@ -1,16 +1,18 @@
 package com.softyorch.beerchat.data.network
 
 import com.google.firebase.database.DatabaseReference
+import com.softyorch.beerchat.data.network.dto.MessageDto
 import javax.inject.Inject
 
 class FirebaseChatService @Inject constructor(private val reference: DatabaseReference) {
 
     companion object {
         private const val PATH = "messages"
+        private const val EXAMPLE_PATH = "examples"
     }
 
-    fun sendMsgToFirebase(msg: String) {
+    fun sendMsgToFirebase(msgDto: MessageDto) {
         val newMsg = reference.child(PATH).push()
-        newMsg.setValue(msg)
+        newMsg.setValue(msgDto)
     }
 }
