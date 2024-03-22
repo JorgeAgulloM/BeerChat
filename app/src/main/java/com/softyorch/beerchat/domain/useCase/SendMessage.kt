8fb106b9b86c47c0725d64da725a15150009db46
Jorge1 <agullojorge@gmail.com>
2024-03-22 18:57:19 +0100

    Example data model

--- app/src/main/java/com/softyorch/beerchat/domain/useCase/SendMessage.kt
+++ app/src/main/java/com/softyorch/beerchat/domain/useCase/SendMessage.kt
@@ -1,10 +1,33 @@
 package com.softyorch.beerchat.domain.useCase
 
 import com.softyorch.beerchat.data.network.FirebaseChatService
+import com.softyorch.beerchat.data.network.dto.MessageDto
+import com.softyorch.beerchat.data.network.dto.UserDto
+import java.util.Calendar
 import javax.inject.Inject
 
 class SendMessage @Inject constructor(private val firebaseChatService: FirebaseChatService) {
     operator fun invoke(msg: String) {
-        firebaseChatService.sendMsgToFirebase(msg)
+        firebaseChatService.sendMsgToFirebase(generateUserDto(msg))
+    }
+
+    private fun generateUserDto(msg: String): MessageDto {
+        val calendar = Calendar.getInstance()
+
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val min = calendar.get(Calendar.MINUTE)
+        val time = "$hour:$min"
+
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val date = "$day-$month-$year"
+
+        val user = UserDto(
+            userName = "UserTest",
+            admin = false
+        )
+
+        return MessageDto(msg = msg, hour = time, date = date, user = user)
     }
 }
