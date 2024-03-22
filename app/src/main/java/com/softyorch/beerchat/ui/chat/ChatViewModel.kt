c84950b8ec2baef609fcbf1f0c93010cc67ffca8
Jorge1 <agullojorge@gmail.com>
2024-03-22 20:09:13 +0100

    Implemented get data from firebase use case

--- app/src/main/java/com/softyorch/beerchat/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/softyorch/beerchat/ui/chat/ChatViewModel.kt
@@ -1,12 +1,42 @@
 package com.softyorch.beerchat.ui.chat
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.beerchat.domain.useCase.GetMessage
 import com.softyorch.beerchat.domain.useCase.SendMessage
+import com.softyorch.beerchat.ui.chat.model.MessageUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ChatViewModel @Inject constructor(private val sendMessage: SendMessage): ViewModel() {
+class ChatViewModel @Inject constructor(
+    private val sendMessage: SendMessage,
+    private val getMessage: GetMessage
+): ViewModel() {
+
+    private val _messageList = MutableStateFlow<List<MessageUi>>(emptyList())
+    val messageList: StateFlow<List<MessageUi>> = _messageList
+
+    init {
+        getMessages()
+    }
+
+    private fun getMessages() {
+        viewModelScope.launch {
+            getMessage().collect { list ->
+                list.forEach {
+                    Log.i("BeerChatLog", "Message: $it")
+                }
+                _messageList.update { list }
+            }
+        }
+    }
+
     fun sendMessage() {
         val msg = "Holiwi"
         sendMessage(msg)
