bc2a5c1ef2dc6919bad487888b3a11bbe1853b86
Jorge1 <agullojorge@gmail.com>
2024-03-22 05:55:37 +0100

    Configured viewBinding

---
+++ app/src/main/java/com/softyorch/beerchat/ui/main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.softyorch.beerchat.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.softyorch.beerchat.R
+import com.softyorch.beerchat.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
