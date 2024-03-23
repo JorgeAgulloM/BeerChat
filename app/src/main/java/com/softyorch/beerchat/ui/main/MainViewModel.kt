a4b43769c1460000ab32832ddbfd39d37997dbdb
Jorge1 <agullojorge@gmail.com>
2024-03-23 09:19:59 +0100

    Implemented loading

--- app/src/main/java/com/softyorch/beerchat/ui/main/MainViewModel.kt
+++ app/src/main/java/com/softyorch/beerchat/ui/main/MainViewModel.kt
@@ -2,18 +2,45 @@ package com.softyorch.beerchat.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.softyorch.beerchat.domain.useCase.GetUserName
 import com.softyorch.beerchat.domain.useCase.SaveUserName
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val saveUserName: SaveUserName,
+    private val getUserName: GetUserName,
     private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
+
+
+    private val _uiState = MutableStateFlow<MainViewState>(MainViewState.LOADING)
+    val uiState: StateFlow<MainViewState> = _uiState
+
+    init {
+        verifyUserLogged()
+    }
+
+    private fun verifyUserLogged() {
+        viewModelScope.launch {
+            val name = async { getUserName() }.await()
+            _uiState.update {
+                if (name.isNotBlank()) {
+                    MainViewState.REGISTERED
+                } else {
+                    MainViewState.UNREGISTERED
+                }
+            }
+        }
+    }
 
     fun saveNickName(userName: String) {
         viewModelScope.launch(dispatcherIO) {
