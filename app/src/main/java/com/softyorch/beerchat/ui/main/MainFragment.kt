93403899527c328e9d89db57f254420200fa3985
Jorge1 <agullojorge@gmail.com>
2024-03-23 08:35:11 +0100

    Save userName

--- app/src/main/java/com/softyorch/beerchat/ui/main/MainFragment.kt
+++ app/src/main/java/com/softyorch/beerchat/ui/main/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.softyorch.beerchat.R
 import com.softyorch.beerchat.databinding.FragmentMainBinding
@@ -14,14 +15,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         binding.btnChat.setOnClickListener {
-            if (!binding.tiedName.text.isNullOrEmpty())
+            if (!binding.tiedName.text.isNullOrEmpty()) {
+                viewModel.saveNickName(binding.tiedName.text.toString())
                 findNavController().navigate(R.id.action_main_fragment_to_chat_fragment)
+            }
         }
 
         return binding.root
