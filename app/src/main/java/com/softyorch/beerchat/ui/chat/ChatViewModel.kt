16ae0906b6f7178c99225d1b159bc068355b1e3a
Jorge1 <agullojorge@gmail.com>
2024-03-23 08:33:12 +0100

    Implemented getUserName use case

--- app/src/main/java/com/softyorch/beerchat/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/softyorch/beerchat/ui/chat/ChatViewModel.kt
@@ -4,9 +4,12 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.softyorch.beerchat.domain.useCase.GetMessage
+import com.softyorch.beerchat.domain.useCase.GetUserName
 import com.softyorch.beerchat.domain.useCase.SendMessage
 import com.softyorch.beerchat.ui.chat.model.MessageUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -16,14 +19,25 @@ import javax.inject.Inject
 @HiltViewModel
 class ChatViewModel @Inject constructor(
     private val sendMessage: SendMessage,
-    private val getMessage: GetMessage
+    private val getMessage: GetMessage,
+    private val getUserName: GetUserName,
+    private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO
 ): ViewModel() {
 
     private val _messageList = MutableStateFlow<List<MessageUi>>(emptyList())
     val messageList: StateFlow<List<MessageUi>> = _messageList
 
+    var name: String = ""
+
     init {
+        getNickName()
         getMessages()
+    }
+
+    private fun getNickName() {
+        viewModelScope.launch(dispatcherIO) {
+            name = getUserName()
+        }
     }
 
     private fun getMessages() {
