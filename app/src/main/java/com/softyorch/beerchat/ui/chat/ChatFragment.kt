3e066ebfd2bbf97e13e4908dca22020d884eaaf7
Jorge1 <agullojorge@gmail.com>
2024-03-23 07:51:42 +0100

    Paint data with adapter

--- app/src/main/java/com/softyorch/beerchat/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/softyorch/beerchat/ui/chat/ChatFragment.kt
@@ -1,21 +1,26 @@
 package com.softyorch.beerchat.ui.chat
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.softyorch.beerchat.R
 import com.softyorch.beerchat.databinding.FragmentChatBinding
+import com.softyorch.beerchat.ui.chat.adapter.ChatAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ChatFragment : Fragment() {
 
     private lateinit var binding: FragmentChatBinding
     private val viewModel by viewModels<ChatViewModel>()
+    private lateinit var chatAdapter: ChatAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +31,32 @@ class ChatFragment : Fragment() {
             findNavController().navigate(R.id.action_chat_fragment_back_main_fragment)
         }
         binding.btnSendChat.setOnClickListener { viewModel.sendMessage() }
+
+        setUpUI()
+
         return binding.root
+    }
+
+    private fun setUpUI() {
+        setUpMessages()
+        subscribeToMessages()
+    }
+
+    private fun setUpMessages() {
+        chatAdapter = ChatAdapter(mutableListOf(), "Yorch")
+        binding.rvMsg.apply {
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun subscribeToMessages() {
+        lifecycleScope.launch {
+            viewModel.messageList.collect { list ->
+                chatAdapter.updateList(list.toMutableList())
+                binding.rvMsg.scrollToPosition(chatAdapter.messagesList.size - 1)
+            }
+        }
     }
 
 }
