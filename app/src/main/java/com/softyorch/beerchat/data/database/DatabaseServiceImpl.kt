d42f09e8dd114e9628623a003969a15b0145f015
Jorge1 <agullojorge@gmail.com>
2024-03-23 08:31:39 +0100

    Created database

---
+++ app/src/main/java/com/softyorch/beerchat/data/database/DatabaseServiceImpl.kt
@@ -0,0 +1,35 @@
+package com.softyorch.beerchat.data.database
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.softyorch.beerchat.domain.interfaces.IDatabaseService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class DatabaseServiceImpl @Inject constructor(private val context: Context) : IDatabaseService {
+
+    companion object {
+        private val USER_NAME = stringPreferencesKey("username")
+    }
+
+    private val Context.userPreferenceDataStore: DataStore<Preferences> by preferencesDataStore(
+        name = "user"
+    )
+
+    override fun getUserName(): Flow<String> {
+        return context.userPreferenceDataStore.data.map { preferences ->
+            preferences[USER_NAME] ?: ""
+        }
+    }
+
+    override suspend fun saveUserName(userName: String) {
+        context.userPreferenceDataStore.edit { preference ->
+            preference[USER_NAME] = userName
+        }
+    }
+}
