9eb20265d9f91544da50e3c4796c9d54c5fea502
Jorge1 <agullojorge@gmail.com>
2024-03-24 11:59:17 +0100

    Show or not other's elements

--- app/src/main/java/com/softyorch/beerchat/ui/chat/adapter/ChatViewHolder.kt
+++ app/src/main/java/com/softyorch/beerchat/ui/chat/adapter/ChatViewHolder.kt
@@ -1,5 +1,8 @@
 package com.softyorch.beerchat.ui.chat.adapter
 
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.marginTop
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.softyorch.beerchat.databinding.ItemMsgMeBinding
@@ -9,33 +12,51 @@ import com.softyorch.beerchat.ui.chat.adapter.ChatAdapter.Companion.SEND_MESSAGE
 import com.softyorch.beerchat.ui.chat.model.MessageUi
 
 class ChatViewHolder(private val binding: ViewBinding):RecyclerView.ViewHolder(binding.root) {
-    fun bind(messageUi: MessageUi, itemViewType: Int) {
+    fun bind(messageUi: MessageUi, oldMessage: MessageUi? = null, itemViewType: Int) {
         when (itemViewType) {
-            SEND_MESSAGE -> bindSendMessage(messageUi)
-            RECEIVE_MESSAGE -> bindReceiveMessage(messageUi)
+            SEND_MESSAGE -> bindSendMessage(messageUi, oldMessage)
+            RECEIVE_MESSAGE -> bindReceiveMessage(messageUi, oldMessage)
         }
     }
 
-    private fun bindReceiveMessage(messageUi: MessageUi) {
+    private fun bindReceiveMessage(messageUi: MessageUi, oldMessage: MessageUi?) {
         val currentBinding = binding as ItemMsgOtherBinding
+        val isLastUser = isLastUser(messageUi.user.userName, oldMessage)
+        val isDiffDate = isDiffDate(messageUi.date, oldMessage)
         messageUi.apply {
             currentBinding.apply {
-                tvDateOther.text = date
-                tvChatMe.text = msg
-                tvName.text = user.userName
+                ivUser.visibility = if (!isLastUser) ImageView.VISIBLE else ImageView.GONE
+                tvName.visibility = if (!isLastUser) TextView.VISIBLE else TextView.GONE
+                tvChat.text = msg
                 tvHour.text = hour
+                if (!isLastUser) { tvName.text = user.userName }
+
+                tvDate.visibility = if (!isDiffDate) TextView.VISIBLE else TextView.GONE
+                if (!isDiffDate) { tvDate.text = date }
             }
         }
     }
 
-    private fun bindSendMessage(messageUi: MessageUi) {
+    private fun bindSendMessage(messageUi: MessageUi, oldMessage: MessageUi?) {
         val currentBinding = binding as ItemMsgMeBinding
+        val isLastUser = isLastUser(messageUi.user.userName, oldMessage)
+        val isDiffDate = isDiffDate(messageUi.date, oldMessage)
         messageUi.apply {
             currentBinding.apply {
-                tvDateMe.text = date
-                tvChatMe.text = msg
+                tvChat.text = msg
                 tvHour.text = hour
+                if (!isDiffDate) {
+                    tvDateMe.text = date
+                }
+                tvDateMe.visibility = if (isDiffDate) TextView.GONE else TextView.VISIBLE
             }
         }
     }
+
+    private fun isLastUser(newUser: String, oldMessage: MessageUi?): Boolean =
+        if (oldMessage == null) false else newUser == oldMessage.user.userName
+
+    private fun isDiffDate(newDate: String, oldMessage: MessageUi?): Boolean =
+        if (oldMessage == null) false else newDate == oldMessage.date
+
 }
